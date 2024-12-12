0033e451b95c6dd8e3ee0e6e68d078e31d253b80
Maksim Umaev <yastey637@gmail.com>
2024-12-12 23:00:35 +0500

    configMySql

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,14 +5,16 @@ namespace crm.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<Record> Records { get; set; } = null!;
-    public DbSet<Master> Masters { get; set; } = null!;
-    public DbSet<Admin> Admins { get; set; } = null!;
+    public DbSet<Record> Records { get; set; }
+    public DbSet<Master> Masters { get; set; }
+    public DbSet<Admin> Admins { get; set; }
 
     public ApplicationDbContext() => Database.EnsureCreated();
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=DESKTOP-5K5VL0B;Database=CRM;Trusted_Connection=True;TrustServerCertificate=True;");
+        // optionsBuilder.UseSqlServer("Server=DESKTOP-5K5VL0B;Database=CRM;Trusted_Connection=True;TrustServerCertificate=True;");
+        optionsBuilder.UseMySql("server=localhost;user=root;password=mysql123;database=crm;", 
+            new MySqlServerVersion(new Version(8, 0, 40)));
     }
 }
